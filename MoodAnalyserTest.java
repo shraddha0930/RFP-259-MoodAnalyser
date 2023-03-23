b61a7dbd38ea556ed9158603b169b2622c11cf83
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-23 22:41:26 +0530

    Added:code to inform user if entered invalid mood

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -6,24 +6,33 @@ import static org.junit.Assert.assertEquals;
 public class MoodAnalyserTest {
     @Test
     public void given_SadMood_Should_Return_Sad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am In a Sad Mood");
-        assertEquals("SAD", mood);
-
+        MoodAnalyser moodAnalyzer = new MoodAnalyser("I am In a Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
-
-   // @Test
-//    public void given_AnyMood_Should_Return_HAPPY() {
-//        MoodAnalyser moodAnalyser = new MoodAnalyser();
-//        String mood = moodAnalyser.analyseMood("I am in any Mood");
-//        assertEquals("HAPPY", mood);
-//    }
 
     @Test
-    public void givenNullMood_ShouldThrowException() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood =moodAnalyser.analyseMood(null);
-        assertEquals("HAPPY",mood);
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
+
+//    @Test
+//    public void givenNullMood_ShouldThrowException() {
+//        MoodAnalyser moodAnalyser = new MoodAnalyser();
+//        String mood =moodAnalyser.analyseMood(null);
+//        assertEquals("HAPPY",mood);
+//    }
 
 }
