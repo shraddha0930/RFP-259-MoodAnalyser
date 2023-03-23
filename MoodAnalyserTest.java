af15371409d59152f1b19374e499c0fe92a88ae1
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-23 20:34:33 +0530

    Added:code to handle exception if user provides invalid mood

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,20 +1,29 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MoodAnalyserTest {
     @Test
     public void given_SadMood_Should_Return_Sad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
         String mood = moodAnalyser.analyseMood("I am In a Sad Mood");
-        Assert.assertEquals("SAD", mood);
+        assertEquals("SAD", mood);
 
     }
 
+   // @Test
+//    public void given_AnyMood_Should_Return_HAPPY() {
+//        MoodAnalyser moodAnalyser = new MoodAnalyser();
+//        String mood = moodAnalyser.analyseMood("I am in any Mood");
+//        assertEquals("HAPPY", mood);
+//    }
+
     @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
+    public void givenNullMood_ShouldThrowException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in any Mood");
-        Assert.assertEquals("HAPPY", mood);
+        String mood =moodAnalyser.analyseMood(null);
+        assertEquals("HAPPY",mood);
     }
 
 }
