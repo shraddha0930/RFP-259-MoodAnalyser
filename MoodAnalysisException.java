7f1824685dcac66a549bdeb7c1968ac0a49c3352
shraddha0930 <shraddhaingole11@gmail.com>
2023-03-23 22:42:08 +0530

    Added:code to inform user if entered invalid mood

---
+++ MoodAnalysisException.java
@@ -0,0 +1,9 @@
+public class MoodAnalysisException extends Exception {
+    public MoodAnalysisException(String message, Exception_Type type) {
+        super(message);
+    }
+
+    public enum Exception_Type {
+        NULL, EMPTY;
+    }
+}
